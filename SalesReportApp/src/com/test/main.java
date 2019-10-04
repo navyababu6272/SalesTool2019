725356553c8f4f0a5d7a669558ed2301a0984e80
navyababu6272 <navyasree183@gmail.com>
2019-10-04 11:15:09 +0530

    Initial Commit

---
+++ SalesReportApp/src/com/test/main.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		salesdata d=new salesdata();
+		System.out.println("hiii guys");
+		displayGreetings();
+		d.display();
+
+	}
+	private static void displayGreetings() {
+		// TODO Auto-generated method stub
+		System.out.println("hiiii welcom");
+		
+	}
+	
+
+}
---
+++ SalesReportApp/src/com/test/salesdata.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class salesdata {
+	void display()
+	{
+		System.out.println("hellooo");
+	}
+
+}
